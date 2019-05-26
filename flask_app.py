3c36b8ad98a53f3c9a2eb7097b8e60be9afc9e9c
Evann <evann.courdier@mines-paristech.fr>
2019-05-26 17:01:05 +0200

    quickfix

--- flask_app.py
+++ flask_app.py
@@ -26,7 +26,8 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == "__main__":
     print("======================== START ==========================")
-    app = create_app()
     app.run(host='0.0.0.0', port=50000, debug=True, threaded=True)
