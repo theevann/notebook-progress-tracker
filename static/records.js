a860170324b9deff3cf685f1eff936353f038c16
Evann <evann.courdier@mines-paristech.fr>
2019-05-30 14:37:26 +0200

    Add filters to records page

--- static/records.js
+++ static/records.js
@@ -2,25 +2,100 @@
 class RecordsList extends React.Component {
     constructor() {
         super();
-        this.state = { 'records': [] };
+        this.state = {
+            'records': [],
+            'filters': {
+                'session_id': '',
+                'sender_name': '',
+                'f_time': '',
+                'question_nb': ''
+            }
+        };
     }
 
     componentDidMount() {
       this.update();
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    }
+
     update() {
         jQuery.get("/get-records", (data) => {
-            console.log(data);
             this.setState({ 'records': data });
         });
     }
 
+    onFilterChange(name, value) {
+        let filters = this.state.filters;
+        filters[name] = value;
+        this.setState({ 'filters': filters });
+    }
+
+    clearFilters(name, value) {
+        let filters = {
+            'session_id': '',
+            'sender_name': '',
+            'f_time': '',
+            'question_nb': ''
+        };
+        this.setState({ 'filters': filters });
+    }
+
     render() {
+        let records = [];
+        let filters = this.state.filters;
+
+        this.state.records.forEach(record => {
+            try {
+                if (!Object.keys(filters).every(key => record[key].toString().match(new RegExp(filters[key], "i")) ))
+                    return;
+                records.push(React.createElement(RecordsRow, {key: record.id, record: record, update: this.update.bind(this)}));
+            } catch (e) {
+                records.push(React.createElement(RecordsRow, {key: record.id, record: record, update: this.update.bind(this)}));
+            }
+
+        })
+
         return [
             React.createElement(RecordsHeader, {key: "header"}),
-            this.state.records.map((record) => React.createElement(RecordsRow, {key: record.id, record: record, update: () => this.update()}))
+            React.createElement(RecordsSearchbar, {key: "searchbar", clearFilters: this.clearFilters.bind(this), onFilterChange: this.onFilterChange.bind(this)}),
+            records
         ];
+    }
+}
+
+
+class RecordsSearchbar extends React.Component {
+    constructor() {
+        super();
+        this.state = { };
+    }
+
+    handleFilterTextChange(e) {
+        this.props.onFilterChange(e.target.name, e.target.value);
+    }
+
+    clearFilters() {
+        document.querySelectorAll(".search input").forEach(input => input.value='');
+        this.props.clearFilters();
+    }
+
+    render() {
+        let fields = [['Session ID', 'session_id'], ['Name', 'sender_name'], ['Date', 'f_time'], ['Question nb', 'question_nb']]
+        return (
+            React.createElement("div", {className: "row search"}, 
+                fields.map(([name, field]) =>
+                    React.createElement("div", {key: name, className: "col-sm-2"}, 
+                        React.createElement("input", {type: "text", name: field, placeholder: name, style: { 'width': '100%'}, onChange: this.handleFilterTextChange.bind(this)})
+                    )
+                ), 
+                React.createElement("div", {className: "col-sm-4"}, 
+                    React.createElement("button", {onClick: this.clearFilters.bind(this), className: "btn btn-secondary"}, "Clear Filters")
+                )
+            )
+        );
     }
 }
 
--- static/src/records.jsx
+++ static/src/records.jsx
@@ -3,25 +3,100 @@
 class RecordsList extends React.Component {
     constructor() {
         super();
-        this.state = { 'records': [] };
+        this.state = {
+            'records': [],
+            'filters': {
+                'session_id': '',
+                'sender_name': '',
+                'f_time': '',
+                'question_nb': ''
+            }
+        };
     }
 
     componentDidMount() {
       this.update();
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    }
+
     update() {
         jQuery.get("/get-records", (data) => {
-            console.log(data);
             this.setState({ 'records': data });
         });
     }
 
+    onFilterChange(name, value) {
+        let filters = this.state.filters;
+        filters[name] = value;
+        this.setState({ 'filters': filters });
+    }
+
+    clearFilters(name, value) {
+        let filters = {
+            'session_id': '',
+            'sender_name': '',
+            'f_time': '',
+            'question_nb': ''
+        };
+        this.setState({ 'filters': filters });
+    }
+
     render() {
+        let records = [];
+        let filters = this.state.filters;
+
+        this.state.records.forEach(record => {
+            try {
+                if (!Object.keys(filters).every(key => record[key].toString().match(new RegExp(filters[key], "i")) ))
+                    return;
+                records.push(<RecordsRow key={record.id} record={record} update={this.update.bind(this)} />);
+            } catch (e) {
+                records.push(<RecordsRow key={record.id} record={record} update={this.update.bind(this)} />);
+            }
+
+        })
+
         return [
             <RecordsHeader key="header" />,
-            this.state.records.map((record) => <RecordsRow key={record.id} record={record} update={() => this.update()} />)
+            <RecordsSearchbar key="searchbar" clearFilters={this.clearFilters.bind(this)} onFilterChange={this.onFilterChange.bind(this)} />,
+            records
         ];
+    }
+}
+
+
+class RecordsSearchbar extends React.Component {
+    constructor() {
+        super();
+        this.state = { };
+    }
+
+    handleFilterTextChange(e) {
+        this.props.onFilterChange(e.target.name, e.target.value);
+    }
+
+    clearFilters() {
+        document.querySelectorAll(".search input").forEach(input => input.value='');
+        this.props.clearFilters();
+    }
+
+    render() {
+        let fields = [['Session ID', 'session_id'], ['Name', 'sender_name'], ['Date', 'f_time'], ['Question nb', 'question_nb']]
+        return (
+            <div className="row search">
+                {fields.map(([name, field]) =>
+                    <div key={name} className='col-sm-2'>
+                        <input type="text" name={field} placeholder={name} style={{ 'width': '100%' }} onChange={this.handleFilterTextChange.bind(this)}></input>
+                    </div>
+                )}
+                <div className='col-sm-4'>
+                    <button onClick={this.clearFilters.bind(this)} className='btn btn-secondary'>Clear Filters</button>
+                </div>
+            </div>
+        );
     }
 }
 
