0c0e0fef1c2136f44f9708348741645b2618402a
Evann <evann.courdier@mines-paristech.fr>
2019-06-10 18:12:20 +0200

    Add signup page

--- routes/login.py
+++ routes/login.py
@@ -1,9 +1,46 @@
-from .base import login_manager, load_user
-from flask_login import login_user, logout_user, login_required, current_user
-from flask import Blueprint, render_template, request, redirect, url_for
+import re
+from .auth import login_manager, load_user
+from models import User, db
 
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash
 
 login_bp = Blueprint('login', __name__)
+
+
+@login_bp.route('/signup', methods=["GET", "POST"])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('sessions.show_sessions'))
+
+    if request.method == "GET":
+        return render_template("signup.html", error="")
+
+    errors = []
+    username = request.form["username"]
+    user = load_user(username)
+    if user is not None:
+        errors += ["Username already in use"]
+
+    password = request.form["password"]
+    if len(password) < 3:
+        errors += ["Password too short"]
+
+    email = request.form["email"]
+    if not check_email(email):
+        errors += ["Email is incorrect"]
+    elif User.query.filter_by(email=email).first():
+        errors += ["A user with this email already exists"]
+
+    if errors:
+        return render_template("signup.html", error=" - ".join(errors))
+
+    user = User(username=username, email=email, password_hash=generate_password_hash(password))
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('login.login'))
 
 
 @login_bp.route('/login', methods=["GET", "POST"])
@@ -29,3 +66,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login.login'))
+
+
+def check_email(email):
+    pattern = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+    return re.match(pattern, email) is not None
