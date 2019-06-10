70d43cc96fa631df4d762b6d316d4ac439f1b1e0
Evann <evann.courdier@mines-paristech.fr>
2019-06-10 22:06:38 +0200

    Create index.html and add explanations

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,18 +1,20 @@
 import os
 from .auth import login_manager
 
+from .index import index_bp
+from .login import login_bp
 from .sessions import sessions_bp
 from .records import records_bp
 from .dashboard import dashboard_bp
-from .login import login_bp
 from .sendcode import sendcode_bp
 
 
 def init_app(app):
+    app.register_blueprint(index_bp)
+    app.register_blueprint(login_bp)
     app.register_blueprint(sessions_bp)
     app.register_blueprint(records_bp)
     app.register_blueprint(dashboard_bp)
-    app.register_blueprint(login_bp)
     app.register_blueprint(sendcode_bp)
     app.secret_key = os.getenv("SECRET_KEY")
     login_manager.init_app(app)
