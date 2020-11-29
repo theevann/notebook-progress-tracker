53cc31180ed1e762dff165646091c12f4971a3ad
Evann Courdier <evann.courdier@epfl.ch>
2020-11-29 16:22:00 +0100

    minor changes

--- flask_app.py
+++ flask_app.py
@@ -6,16 +6,14 @@ import models
 import utils
 
 
-# TODO: make an owner page, protect by password, add more fields
+# TODO: make an owner page, add more fields, max Q number
 # Parameters : On/off switch latex, latex font-size
 # Add dashboard page: List of sessions on the top, screen split in two with last records on the left, clickable bar graph with num people/question
 # For records page : split record by session, allow search/filtering by people, add correct answer and check
 # For sessions page : Add nb participants (https://docs.sqlalchemy.org/en/13/orm/tutorial.html#using-subqueries), are you sure delete button
 # Other views : Follow one participant in a session, see number of participants who have answer each questions as a bar graph
 # DDOS / Overflow protection (check IP sent ratio over last minute)
-# More data types: plot
 # All live with server push : https://stackoverflow.com/questions/12232304/how-to-implement-server-push-in-flask-framework
-# React
 
 
 def create_app():
@@ -33,4 +31,4 @@ migrate = Migrate(app, models.db)
 
 if __name__ == "__main__":
     print("======================== START ==========================")
-    app.run(host='0.0.0.0', port=50000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=50001, debug=True, threaded=True)
