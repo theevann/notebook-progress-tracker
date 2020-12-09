fd14891c5a39f008660a6ca9cd274cea8b7dd779
Aymeric <aymeric.dieuleveut@gmail.com>
2020-12-09 22:54:27 +0100

    add remove todos in ntp-jpt

--- npt-jpt/main.js
+++ npt-jpt/main.js
@@ -13,8 +13,33 @@ define([
 
         var full_action_name = Jupyter.actions.register(action, action_name, prefix);
         Jupyter.toolbar.add_buttons_group([full_action_name]);
+
+        var action2 = {
+            icon: 'fa-eye-slash',
+            help: 'NPT - Create student initial version',
+            help_index: 'zz',
+            handler: create_init
+        };
+        var prefix = 'npt';
+        var action2_name = 'create-student-initial-version';
+
+        var full_action2_name = Jupyter.actions.register(action2, action2_name, prefix);
+        Jupyter.toolbar.add_buttons_group([full_action2_name]);
     }
 
+    function create_init(){
+        let path = Jupyter.notebook.notebook_path.replace(".ipynb", "_init_student.ipynb");
+        let model_json = Jupyter.notebook.toJSON();
+
+        model_json.cells.forEach((c) => {
+            let text = c.source;
+            c.source = text.replace(/#+\s*TO\s*DO(.|\n)*?(#+\s*END\s*TO\s*DO)/g, "\n#\n#\n# YOUR CODE HERE\n#\n#\n");
+        });
+
+        var model = { type: "notebook", content: model_json };
+
+        Jupyter.notebook.contents.save(path, model);
+    }
 
     function renumber() {
         let nb = 0;
