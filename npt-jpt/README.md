0c5c3fff3b8ebe3b78ff6cd13a4cdb70a2df1782
Evann Courdier <evann.courdier@epfl.ch>
2020-12-10 12:53:22 +0400

    Keep indentation for todos and add configurable parameters - Other minor improvements

--- npt-jpt/README.md
+++ npt-jpt/README.md
@@ -5,4 +5,6 @@ This jupyter extension adds two buttons to the toolbar.
 
 Upon clicking on the renumbering button, the extension finds all send(obj, N) functions in the notebook and renumber them in increasing order starting from 0.
 
-Upon clicking on the init_student_version button, the extension finds all code in between "### TO DO" and "### END TO DO" and replaces it by "# YOUR CODE HERE" (any number of # is acceptable). The new notebook is saved in the same folder with a suffix "init_student_version". 
+Upon clicking on the init_student_version button, the extension finds all code in between "### TO DO" and "### END TO DO" and replaces it by "# YOUR CODE HERE" (any number of # is acceptable). The new notebook is saved in the same folder with a suffix "init_student_version".
+
+These parameters can be changed in the Nbextensions Configuration Panel.
--- npt-jpt/main.js
+++ npt-jpt/main.js
@@ -2,51 +2,53 @@ define([
     'base/js/namespace'
 ], function (Jupyter) {
     function load_ipython_extension() {
+        var prefix = 'npt';
+
         var action = {
             icon: 'fa-sort-numeric-asc',
             help: 'NPT - Renumber send',
             help_index: 'zz',
             handler: renumber
         };
-        var prefix = 'npt';
         var action_name = 'renumber-send';
 
-        var full_action_name = Jupyter.actions.register(action, action_name, prefix);
-        Jupyter.toolbar.add_buttons_group([full_action_name]);
-
+        
         var action2 = {
             icon: 'fa-eye-slash',
             help: 'NPT - Create student initial version',
             help_index: 'zz',
             handler: create_init
         };
-        var prefix = 'npt';
         var action2_name = 'create-student-initial-version';
-
-        var full_action2_name = Jupyter.actions.register(action2, action2_name, prefix);
-        Jupyter.toolbar.add_buttons_group([full_action2_name]);
+        
+        var full_action = Jupyter.actions.register(action, action_name, prefix);
+        var full_action2 = Jupyter.actions.register(action2, action2_name, prefix);
+        Jupyter.toolbar.add_buttons_group([full_action, full_action2]);
     }
 
     function create_init(){
-        let path = Jupyter.notebook.notebook_path.replace(".ipynb", "_init_student_version.ipynb");
+        let replace_text = Jupyter.notebook.config.data.text_replace_todo || "# YOUR CODE HERE"
+        let prefix = Jupyter.notebook.config.data["file_prefix"] || "";
+        let suffix = Jupyter.notebook.config.data["file_suffix"] || "_init_student_version";
+        
+        let path = prefix + Jupyter.notebook.notebook_path.replace(".ipynb", suffix + ".ipynb");
         let model_json = Jupyter.notebook.toJSON();
+        let model = { type: "notebook", content: model_json };
 
-        model_json.cells.forEach((c) => {
-            let text = c.source;
-            c.source = text.replace(/#+\s*TO\s*DO(.|\n)*?(#+\s*END\s*TO\s*DO)/g, "\n#\n#\n# YOUR CODE HERE\n#\n#\n");
+        model_json.cells.forEach(c => {
+            c.source = c.source.replace(/( *)#+ *TO *DO(.|\n)*?(#+ *END *TO *DO)/g,
+                                        (_, s) => `${s}\n${s}#\n${s}#\n${s}${replace_text}\n${s}#\n${s}#\n${s}`);
         });
-
-        var model = { type: "notebook", content: model_json };
 
         Jupyter.notebook.contents.save(path, model);
     }
 
     function renumber() {
-        let nb = 0;
+        let nb = Jupyter.notebook.config.data["starting_question_number"] || 0;
 
         Jupyter.notebook.get_cells().forEach(cell => {
             let text = cell.get_text();
-            let new_text = text.replace(/send\(\s*(\S+.*?)\s*,\s*(\d+)\s*\)/g, (_, obj) => `send(${obj}, ${nb++})`);
+            let new_text = text.replace(/send\( *(\S.*) *, *(\d+) *\)/g, (_, obj) => `send(${obj}, ${nb++})`);
             
             if (text !== new_text)
                 cell.set_text(new_text);
